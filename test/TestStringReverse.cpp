afb525473364addbbe8e46722a0f0733c4166474
Michael Prantl <michael.prantl@hotmail.de>
2019-06-11 22:56:24 +0200

    Implement StringReverse class

--- test/TestStringReverse.cpp
+++ test/TestStringReverse.cpp
@@ -11,5 +11,15 @@ TEST_CASE("A default constructed StringReverse is empty", "[stringreverse]") {
 
 TEST_CASE("A StringReverse constructed with 'Hello World' becomes 'dlroW olleH'", "[stringreverse]") {
     const StringReverse s{"Hello World"};
-    //TODO: test it
+    CHECK_FALSE(s == "Hello World");
+    CHECK(s == "dlroW olleH");
+    CHECK_FALSE(s.empty());
+}
+
+TEST_CASE("Moving out of a StringReverse should clear the original object", "[stringreverse]") {
+    StringReverse s{"Hello World"};
+    std::string d = std::move(s);
+    CHECK_FALSE(d.empty());
+    CHECK(s.empty());
+    CHECK(d == "dlroW olleH");
 }
